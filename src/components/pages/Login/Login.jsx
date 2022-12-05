8adc0b306f39591e2ee46cca06cf987315d1cdaa
raihan-778 <raihanarif778@gmail.com>
2022-12-05 15:44:08 +0600

    some bug fixed

--- src/components/pages/Login/Login.jsx
+++ src/components/pages/Login/Login.jsx
@@ -5,11 +5,6 @@ import { AuthContext } from "../../../context/AuthProvider";
 
 const Login = () => {
   const { user, loading, setLoading, login } = useContext(AuthContext);
-  if (loading) {
-    return (
-      <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-violet-400"></div>
-    );
-  }
 
   const navigate = useNavigate();
   const location = useLocation();
