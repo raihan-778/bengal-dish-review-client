b1ab533385b684950aef5d11fcf86c4ad39ad288
raihan-778 <raihanarif778@gmail.com>
2023-01-21 16:42:06 +0600

    some text changes

--- src/components/FeaturesCard/FeaturesCard.jsx
+++ src/components/FeaturesCard/FeaturesCard.jsx
@@ -7,7 +7,7 @@ const FeaturesCard = () => {
         <div className="container max-w-xl p-6 py-12 mx-auto space-y-24 lg:px-8 lg:max-w-7xl">
           <div>
             <h2 className="text-3xl font-bold tracking-tight text-center sm:text-5xl dark:text-gray-50">
-              Our Happy Customer are Our Strength.
+              Our Happy Customers Are Our Strength.
             </h2>
           </div>
 
